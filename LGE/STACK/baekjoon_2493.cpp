4adafc0af656293a21bf77006c05f6e983005dd9
aswo117 <aswo117@naver.com>
2024-03-05 21:02:28 +0900

    Update baekjoon_2493.cpp
    입력은 Pair로, (주소, 값)
    출력은  Queue로

--- LGE/STACK/baekjoon_2493.cpp
+++ LGE/STACK/baekjoon_2493.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <stack>
 #include <queue>
+#include <algorithm>
+
 using namespace std;
 
 int N;//건물 수
-// int H[500000];//탑 높이
-int result[500000];//탑 높이
 
-stack<int> st_input;
+stack<pair<int, int>> st_input;
 queue<int> queue_output;
-
-// void print_input(void){
-// 	for(int i=N-1; i>=0; i--){
-// 		cout<<"H["<<i<<"] : "<<H[i]<<endl;
-// 	}
-// }
 
 void print_result(void){
 	while(!queue_output.empty()){
@@ -23,42 +17,36 @@ void print_result(void){
 	}
 }
 
-void Input_Data(void){
+void Solution(void){
 	cin >> N;
 	// cout<<"[+][sung] N : "<<N<<endl;
 	for (int i = 0; i < N; i++){
 		int data;
 		cin >> data;
-		if(st_input.empty() == true){
-			// cout<<"[+][sung]empty"<<endl;
-			queue_output.push(0);
-			st_input.push(data);
-			// cout<<"0"<<endl;
-		}
-		else{
-			if(data < st_input.top()){
-				// cout<<"[+][sung] < : "<<queue_output.size()<<endl;
-				queue_output.push(queue_output.size());	
-				st_input.push(data);
-			}
-			else if(data == st_input.top()){
-				// cout<<"[+][sung] == : "<<queue_output.back()<<endl;
-				queue_output.push(queue_output.back());
-				st_input.push(data);
+		while(1){
+			if(st_input.empty() == true){
+				st_input.push(make_pair(i+1, data));
+				queue_output.push(0);
+				break;
 			}
 			else{
-				// cout<<"[+][sung] > : "<<queue_output.back()<<endl;
-				queue_output.push(queue_output.back());
-				st_input.push(data);
+				// cout<<"st_input.top().first : "<<st_input.top().first<<endl;
+				// cout<<"st_input.top().second : "<<st_input.top().second<<endl;
+				if(data < st_input.top().second){
+					queue_output.push(st_input.top().first);
+					st_input.push(make_pair(i+1, data));
+					break;
+				}
+				else{
+					st_input.pop();
+				}
 			}
 		}
 	}
 }
 
 int main(){
-	Input_Data();		//	입력 함수
+	Solution();
 	print_result();
 	return 0;
 }
-
-//출력 오류
