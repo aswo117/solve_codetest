33883084ecf3aa8f8fdc53bc382ee1f8910d7521
aswo117 <aswo117@naver.com>
2024-03-26 16:13:14 +0900

    Update baekjoon_6593.py
    완료우

--- LGE/BFS/Python/baekjoon_6593.py
+++ LGE/BFS/Python/baekjoon_6593.py
@@ -1,53 +1,73 @@
 from collections import deque
 
-NZ, NX, NY = map(int, input().split())
-print(f"NZ ,NX, NY = {NZ, NX, NY}")
-q = deque()
+Escaped_flag = False
 
+while(1):
+	NZ, NX, NY = map(int, input().split())
+	if NZ == 0 and NX == 0 and NY == 0:
+		break;
+	# print(f"NZ ,NX, NY = {NZ, NX, NY}")
+	q = deque()
 
-# maps = [[0 for col in range(NY)] for row in range(NX)]
-maps2 = [0]*NZ
-# maps = [[0 for col in range(NY)] for row in range(NX)] for depth in range(NZ)
+	maps2 = [0]*NZ
+	s_index = []
+	e_index = [] #필용벗음
 
-# a.append(maps)
-# a.append(maps)
-# a.append(maps)
-# print(a)
+	# print(q)
+	def print_maps():
+		for z in range(NZ):
+			for i in range(NX):
+				print(f"{maps2[z][i]}")
+			print(f" ")
 
-for z in range(NZ):
-	maps = [[0 for col in range(NY)] for row in range(NX)]
-	for i in range(NX+1):
-		temp = list(map(str, input()))
-		if(temp == []):
-			print("continue")
-			continue
-		else:
-			maps[i] = temp
-			# print(f"maps[{i}] = {maps[i]}")
-	print(f"maps = {maps}, z = {z}")
-	maps2[z] = maps
-	print(f"maps2[0] = {maps2[0]}")
-	print(f"maps2[1] = {maps2[1]}")
-	print(f"maps2[2] = {maps2[2]}")
+	#입력값 받고, S, E 찾기
+	for z in range(NZ):
+		maps = [[0 for col in range(NY)] for row in range(NX)]
+		for i in range(NX+1):
+			temp = list(map(str, input()))
+			if(temp == []):
+				maps2[z] = maps
+			elif 'S' in temp:
+				s_index = [z, i, temp.index('S')]
+				# print(f"S index = {s_index}")
+				q.append((z, i, temp.index('S'), 1))
+				maps[i] = temp
+			elif 'E' in temp:
+				e_index = [z, i, temp.index('E')]
+				# print(f"e index = {e_index}")
+				maps[i] = temp
+			else:
+				maps[i] = temp
+
+	maps2[int(s_index[0])][int(s_index[1])][int(s_index[2])] = 1
+
+	# print_maps()
 	
-print(maps2)
-# for z in range(NZ):
-# 	for x in range(NX):
-# 		print(maps2[z][x])
-# 	print()
+	#탐색
+	while q:
+		z, x, y, d = q.popleft()
+		for dz, dx, dy in((-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)):
+			nz, nx, ny = z + dz, x + dx, y + dy
+			if not (0<=nz<NZ) or not (0<=nx<NX) or not (0<=ny<NY):
+				continue
+			if maps2[nz][nx][ny] != '.':
+				if maps2[nz][nx][ny] == 'E':
+					maps2[nz][nx][ny] = d
+					print(f"Escaped in {maps2[nz][nx][ny]} minute(s).")
+					q.clear()
+					Escaped_flag = True
+					break
+				continue
 
-# for z in range(NZ):
-# 	for i in range(NX):
-# 		temp = list(map(str, input()))
-# 		if temp == []:
-# 			continue
-# 		else:
-# 			maps[z][i] = temp
+			maps2[nz][nx][ny] = d + 1
+			q.append((nz, nx, ny, d+1))
+			
+	if Escaped_flag is False:
+		print(f"Trapped!")
+		q.clear()
+	Escaped_flag = False
 		
-# print(maps)
-# print(maps[2][3][4])
 
 
-# for i in range(NZ):
-# 	print(maps[i])
-# 	print()
+	#출력
+	# print_maps()
