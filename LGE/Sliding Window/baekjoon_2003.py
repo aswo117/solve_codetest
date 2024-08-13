ea199e1980da3e2fae827beeeea83f1e955930f0
aswo117 <aswo117@naver.com>
2024-08-13 21:22:11 +0900

    Update baekjoon_2003.py

--- LGE/Sliding Window/baekjoon_2003.py
+++ LGE/Sliding Window/baekjoon_2003.py
@@ -1,2 +1,14 @@
 #https://www.acmicpc.net/problem/2003
 
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+sub = [0] * (N+1)
+for index, val in enumerate(A):
+  sub[index+1] = val+sub[index]
+cnt = 0
+for end in range(1, N+1):
+  for start in range(end):
+    if sub[end]-sub[start] == M:
+      cnt += 1
+print(cnt)
