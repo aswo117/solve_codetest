b5f122e0b0999c85befbc5c3aee5e7de988a4eed
aswo117 <aswo117@naver.com>
2024-08-14 11:02:00 +0900

    Update baekjoon_16472.py

--- LGE/Sliding Window/baekjoon_16472.py
+++ LGE/Sliding Window/baekjoon_16472.py
@@ -1,33 +1,31 @@
-#https://www.acmicpc.net/problem/16472 (build err)
+#https://www.acmicpc.net/problem/16472 (70% fail)
 
 import sys
 
 N = int(sys.stdin.readline())
 input_data = list(sys.stdin.readline().strip())
 
-start, end = 0, 1
+start, end = 0, 0
 cur_alpha = []
 cur_alpha.append(input_data[0])
 max_count = 0
+check_count = 0
 
-while start < len(input_data) and end < len(input_data):
-	print(f"N = {N}")
-	print(f"len(input_data) = {len(input_data)}")
-	print(f"max_count = {max_count}")
-	print(f"cur_alpha = {cur_alpha}")
-	if N >= len(input_data):
+while start < len(input_data)-1 and end < len(input_data)-1:
+	if N > check_count and end <= len(input_data):
 		end = end+1
 		if input_data[end] not in cur_alpha:
-			cur_alpha.append(input_data[end])
+			check_count = check_count + 1
+		cur_alpha.append(input_data[end])
 	else:
-		if max_count < len(cur_alpha):
-			max_count = len(cur_alpha)
+		if max_count < len(cur_alpha)-1:
+			max_count = len(cur_alpha)-1
 		start = start+1
 		end = start
-		print(f"input_data[start] = {input_data[start]}")
-		print(type(input_data[start]))
-		cur_alpha = cur_alpha[input_data[start]]
+		check_count = 0
+		cur_alpha = [input_data[start]]
 
 print(max_count)
+
 
 
