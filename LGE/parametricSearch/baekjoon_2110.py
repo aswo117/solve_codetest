87ae39d855d6ba7b703e43f87024a6985dd4a717
aswo117 <aswo117@naver.com>
2024-08-29 10:57:20 +0900

    Update baekjoon_2110.py

--- LGE/parametricSearch/baekjoon_2110.py
+++ LGE/parametricSearch/baekjoon_2110.py
@@ -1,53 +1,58 @@
 import sys
 from collections import deque
+import math
 
 input = sys.stdin.readline
 
-# print(f"[+][sung] ")
+#print(f"[+][sung]")
 
 N, C = map(int, input().split())
 data_list = list()
-dist_list = list()
-solve = 0
 
 for i in range(N):
 	data_list.append(int(input()))
+
 data_list.sort()
-# print(N, C, data_list)
-for j in range(N-1):
-	dist_list.append((data_list[j]-data_list[j+1])*-1)
+# print(data_list)
 
-def check(m):#공유기의 거리가 m
-	temp = 0
-	if data_list[-1] < m: #공유기의 처리 거리가 전체 집거리보다 클 경우 일단 거르고
-		return 0
+def check(m):
+	count = 1 #공유기 개수
+	# dist_m = 0
+	start_point = 0
 	
-	print(f"[+][sung] dist_list = {dist_list}")
-	remain_dist = m
-	print(f"[+][sung] remain_dist = {remain_dist}")
-	while 1: #1번 집을 시작으로 공유기의 처리 거리가 끝날때 까지 반복
-		for i in dist_list: #각 집마다의 거리를 빼주기위한 배열
-			print(f"[+][sung] {remain_dist} = {remain_dist} - {i}")
-			remain_dist = remain_dist - i #남은 거리를 뽑아서			
-			if remain_dist <= 0: #더이상 못 뽑으면
-				return temp
-			else: #뽑을 수 있으면 공유기 개수 추가 한 뒤 다음 집까지 간다.
-				temp = temp+1
+	# print("check start")
+	# print(f"[+][sung] m = {m}")
+	
+	for idx, i in enumerate(data_list):
+		# print(f"[+][sung] {m} - ({data_list[idx]} - {data_list[start_point]}) = {m - (data_list[idx] - data_list[start_point])} ")
+		if m - (data_list[idx] - data_list[start_point]) <= 0:	#공유기 사이의 거리가 집사이의 거리보다 작으면
+			# print("[+][sung] count ++")
+			count = count+1
+			start_point = idx
+	# print(f"[+][sung] count = {count}")
+	return count
 
-S = 0
+S = 1
 E = 1000000000
+max_mid = 0
 
-while S<=E:
-	mid = (S+E)//2
-	total = check(mid) #return 값은 공유기 설치 숫자
+while S<=E:	
+	mid = (S+E)//2		#공유기 사이의 거리
+	total = check(mid)
 	
-	print(f"[+][sung] total = {total}")
-	if total >= C: #같으면 최적값임.
-		solve = mid
-	elif total > C:
+	if total == 0:		#total : 설치된 공유기의 개수
+		# print(f"[+][sung] total = 0")
+		E = mid
+		continue
+	elif total > C:	#설치 해야하는 공유기 개수보다 믾이 설치했으므로
+		# print(f"[+][sung] total > C")
 		S = mid+1
+	elif total == C:	#설치해야 하는 공유기 개수인 C 
+		# print(f"[+][sung] total == C")
+		if max_mid <= mid:
+			max_mid = mid
+		break
 	else:
 		E = mid-1
 	
-
-	
+print(max_mid)
