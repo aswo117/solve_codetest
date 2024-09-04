e6fd789ceed7d628d1f41c5cf3151276beca7e6d
aswo117 <aswo117@naver.com>
2024-09-04 17:24:07 +0900

    Update 09_23__22_7차_2.py

--- LGE/preTest/09_23__22_7차_2.py
+++ LGE/preTest/09_23__22_7차_2.py
@@ -18,13 +18,15 @@ N, info = Input_Data() # 입력 받는 부분
 time_list = list()
 
 for i in range(N):
-	time_list.append((info[i][0], info[i][0] + info[i][1] + 2))
+	time_list.append((info[i][0], info[i][0] + info[i][1] + 2)) # End 시간은 처음시간 + 걸리는 시간 + 휴식시간(2시간)
 # print(time_list)
+
+time_list.sort()
 
 compare_time = time_list[0][1]
 for i in range(1, N):
-	if compare_time <= time_list[i][0]: # 수업 들을 수 있음
-		compare_time = time_list[i][1]
+	if compare_time <= time_list[i][0]: # 들은 수업의 시작시간과 다음 수업의 끝시간비교 시 다음 수업이 같거나 크면 수업 들을 수 있음
+		compare_time = time_list[i][1]    # 비교시간은(Start 시간) 들을 수 있는 수업의 끝시간
 		ans = ans+1
 		# print(f"compare_time = {compare_time}")
  
